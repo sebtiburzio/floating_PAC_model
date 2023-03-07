6b447a408f0c685432ed154e9e8adfd697c3e99a
Seb <sebtiburzio@gmail.com>
2023-03-07 10:29:27 +0100

    Add serialisation of functions

--- floating_PAC_model.py
+++ floating_PAC_model.py
@@ -5,6 +5,7 @@ import time
 import sympy as sm
 import matplotlib.pyplot as plt
 import numpy as np
+import dill
 
 #%% 
 # Funcs
@@ -25,23 +26,20 @@ def plot_FK(f_FK, q_repl, p_repl):
 #%%
 # Init
 
-# parameters
+# constant parameters
 m, g, L, D = sm.symbols('m g L D')
 p = sm.Matrix([m, g, L, D])
-p_vals = [1.0, 9.81, 1.0, 0.1]
 
 # configuration variables
 theta_0, theta_1, x, z, phi = sm.symbols('theta_0 theta_1 x z phi')
 q = sm.Matrix([theta_0, theta_1, x, z, phi])
-q_vals = [1.0, 1.0, 0.0, 0.0, 0.0]
 dtheta_0, dtheta_1, dx, dz, dphi = sm.symbols('dtheta_0 dtheta_1 dx dz dphi')
 dq = sm.Matrix([dtheta_0, dtheta_1, dx, dz, dphi])
-dq_vals = [0.0, 0.0, 0.0, 0.0, 0.0]
-alpha = sm.symbols('alpha') # tip orientation in object base frame
 
 # object coordinates in global frame (forward kinematics)
 fk_x, fk_z = sm.symbols('fk_x fk_z')
 fk = sm.Matrix([fk_x, fk_z])
+alpha = sm.symbols('alpha') # tip orientation in object base frame
 
 # integration variables
 s, v, d = sm.symbols('s v d')
@@ -59,8 +57,6 @@ fk[1] = -L*sm.integrate(sm.sin(alpha),(v, 0, s)) # -ve to match rotation sense i
 rot_phi = sm.rot_axis3(phi)[:2,:2]                  # +ve rotations around robot Y-axis
 rot_alpha = sm.rot_axis3(alpha.subs(v,s))[:2,:2]    #
 fk = sm.Matrix([x, z]) + rot_phi@(fk + D*rot_alpha@sm.Matrix([0, d]))
-
-# plot_FK(fk, dict(zip(q,q_vals)), dict(zip(p,p_vals))) # Test FK
 
 toc = time.perf_counter()
 print("FK gen time: " + str(toc-tic))
@@ -102,7 +98,6 @@ for i in range(5):
         for k in range(5):
             Christoffel = 0.5*(sm.diff(B[i,j],q[k]) + sm.diff(B[i,k],q[j]) - sm.diff(B[j,k],q[i]))
             C[i,j] = C[i,j] + Christoffel*dq[k]
-# # TODO - check if this is correct
 
 toc = time.perf_counter()
 print("C gen time: " + str(toc-tic))
@@ -115,10 +110,25 @@ f_G = sm.lambdify((q,p), G)
 f_B = sm.lambdify((q,p), B)
 f_C = sm.lambdify((q,p,dq), C)
 
+#%% 
+# Save functions to file
+
+dill.dump(f_FK, open("f_FK", "wb"))
+dill.dump(f_G, open("f_G", "wb"))
+dill.dump(f_B, open("f_B", "wb"))
+dill.dump(f_C, open("f_C", "wb"))
+
 #%%
 # Test output
+
+p_vals = [1.0, 9.81, 1.0, 0.1]
+q_vals = [1.0, 1.0, 0.0, 0.0, 0.0]
+dq_vals = [0.0, 0.0, 0.0, 0.0, 1.0]
 
 # print(f_FK(q_vals,p_vals,0.5,0.0))
 # print(f_G(q_vals,p_vals))
 # print(f_B(q_vals,p_vals))
 # print(f_C(q_vals,p_vals,dq_vals))
+
+# plot_FK(fk, dict(zip(q,q_vals)), dict(zip(p,p_vals))) # Test FK
+
