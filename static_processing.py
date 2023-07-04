7a79fedceca4b5653178275bb6720b72216829d4
Seb Tiburzio <sebtiburzio@gmail.com>
2023-07-04 21:07:23 +0200

    Output marker locations for SS analysis in matlab

--- static_processing.py
+++ static_processing.py
@@ -3,14 +3,11 @@
 import os
 import pickle
 import csv
-import cv2
 import numpy as np
 import mpmath as mp
 import sympy as sm
-from scipy import signal
 from scipy.spatial.transform import Rotation as R
 import matplotlib.pyplot as plt
-from matplotlib.animation import FFMpegWriter
 
 def get_FK(q_repl,num_pts=21):
     s_evals = np.linspace(0,1,num_pts)
@@ -116,7 +113,7 @@ RMat_EE = np.array([[O_T_EE[:,0], O_T_EE[:,1],O_T_EE[:,2]],
                     [O_T_EE[:,4], O_T_EE[:,5],O_T_EE[:,6]],
                     [O_T_EE[:,8], O_T_EE[:,9],O_T_EE[:,10]]]).T
 RPY_EE = R.from_matrix(RMat_EE).as_euler('xzy', degrees=False) 
-Gamma = RPY_EE[:,2]
+Gamma = RPY_EE[:,2] # Note this is the rotated angle of the robot - simualted gravity direction would be -ve this
 
 # Import marker positions
 X_base_meas = markers[:,0]
@@ -126,19 +123,19 @@ Z_mid_meas = markers[:,3]
 X_end_meas = markers[:,4]
 Z_end_meas = markers[:,5]
 
-p_vals = [0.4, 0.23, 0.75, 0.012] # cable properties: mass (length), mass (end), length, radius
+p_vals = [0.4, 0.23, 0.75, 0.015] # cable properties: mass (length), mass (end), length, radius
 
 #%%
 # Transform marker points to fixed PAC frame (subtract X/Z, rotate back phi)
 fk_targets_mid = np.vstack([X_mid_meas-X_base_meas,Z_mid_meas-Z_base_meas]).T
 fk_targets_end = np.vstack([X_end_meas-X_base_meas,Z_end_meas-Z_base_meas]).T
 fk_targets = np.hstack([rot_XZ_on_Y(fk_targets_mid,-Gamma), rot_XZ_on_Y(fk_targets_end,-Gamma)])
-# fk_targets = np.hstack([fk_targets_mid, fk_targets_end])
 
 #%%
 # Iterate IK over data
 theta_extracted = np.zeros((fk_targets.shape[0],2,))
-theta_guess = np.array([9.5,-14.5])
+# theta_guess = np.array([9.5,-14.5]) # For black cable
+theta_guess = np.array([11,-19]) # For orange cable
 IK_converged = np.zeros((fk_targets.shape[0],1,))
 
 for n in range(fk_targets.shape[0]):
@@ -153,6 +150,7 @@ Theta1 = theta_extracted[:,1]
 # Plot result over fk_targets
 for i in range(len(theta_extracted)):
     plot_FK(theta_extracted[i],i)
+plt.close()
 
 # %%
 # Export to csv for matlab
@@ -160,7 +158,9 @@ if not os.path.exists(data_dir + '/data_out'):
             os.makedirs(data_dir + '/data_out')
 with open(data_dir + '/data_out/theta_equilibria.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(['Gamma', 'Theta0', 'Theta1'])
+    writer.writerow(['Gamma', 'Theta0', 'Theta1', 'X_mid', 'Z_mid', 'X_end', 'Z_end'])
     for n in range(len(Gamma)):
-        writer.writerow([Gamma[n], Theta0[n], Theta1[n]])
+        writer.writerow([Gamma[n], Theta0[n], Theta1[n], 
+                        fk_targets[n,0], fk_targets[n,1], 
+                        fk_targets[n,2], fk_targets[n,3]])
 # %%
