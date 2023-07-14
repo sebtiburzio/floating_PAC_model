41ab477261835c321b035b8ae039e97ca53c6b1f
Seb <sebtiburzio@gmail.com>
2023-07-14 17:10:02 +0200

    Minor correction

--- image_processing.py
+++ image_processing.py
@@ -60,7 +60,7 @@ def plotim(idx,calib_vis=False):
         ax.plot(base_links[0,:],base_links[1,:],lw=3,c='slategrey')
 
 def plot_markers(idx, plot_mask=True, save=False):
-    img_name = os.listdir(img_dir)[idx]
+    img_name = imgs[idx]
     img = cv2.cvtColor(cv2.imread(img_dir + img_name), cv2.COLOR_BGR2RGB)
 
     fig = plt.figure(figsize=(7.5, 6))
@@ -109,8 +109,8 @@ def UV_to_XZplane(u,v,Y=0):
 
 #%%
 # Paths
-dataset_name = 'black_swing'
-data_date = '0619'
+dataset_name = 'background_test'
+data_date = '1407'
 data_dir = os.getcwd() + '/paramID_data/' + data_date + '/' + dataset_name
 
 print('Dataset: ' + dataset_name)
@@ -119,7 +119,7 @@ print('Path: ' + data_dir)
 
 #%%
 # Camera intrinsic and extrinsic transforms
-with np.load(data_dir + '/TFs.npz') as tfs:
+with np.load(data_dir + '/TFs_adj.npz') as tfs:
     P = tfs['P']
     E_base = tfs['E_base']
     E_cam = tfs['E_cam']
@@ -139,18 +139,18 @@ plotim(0,True)
 
 #%%
 # Define regions of interest
-R_row_start = 600
-R_row_end = 650
-R_col_start = 525
-R_col_end = 550
-G_row_start = 450
-G_row_end = 800
-G_col_start = 900
-G_col_end = 1100
-B_row_start = 50
+R_row_start = 0
+R_row_end = 1080
+R_col_start = 0
+R_col_end = 1920
+G_row_start = 0
+G_row_end = 1080
+G_col_start = 0
+G_col_end = 1920
+B_row_start = 0
 B_row_end = 1080
-B_col_start = 1150
-B_col_end = 1550
+B_col_start = 0
+B_col_end = 1920
 # Set Y positions of markers
 base_Y = EE_start_XYZ[1] + 0.01
 mid_Y = EE_start_XYZ[1] + 0.01
@@ -188,7 +188,7 @@ mid_pos_px = np.array([500,950])
 end_pos_px = np.array([100,1200])
 
 count = 0
-test_max = 1e2
+test_max = 1e9
 for img_name in imgs:
     img = cv2.imread(img_dir + img_name)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -231,47 +231,47 @@ for img_name in imgs:
     masked_G = cv2.bitwise_and(img,img,mask=mask_G)
     masked_B = cv2.bitwise_and(img,img,mask=mask_B)
 
-    # Locate markers at COM of mask
-    base_pos_px = np.array([0,0]) if np.any(np.isnan(center_of_mass(mask_R))) else np.round(center_of_mass(mask_R)).astype(int)
-    mid_pos_px = np.array([0,0]) if np.any(np.isnan(center_of_mass(mask_G))) else np.round(center_of_mass(mask_G)).astype(int)
-    end_pos_px = np.array([0,0]) if np.any(np.isnan(center_of_mass(mask_B))) else np.round(center_of_mass(mask_B)).astype(int)
+    # # Locate markers at COM of mask
+    # base_pos_px = np.array([0,0]) if np.any(np.isnan(center_of_mass(mask_R))) else np.round(center_of_mass(mask_R)).astype(int)
+    # mid_pos_px = np.array([0,0]) if np.any(np.isnan(center_of_mass(mask_G))) else np.round(center_of_mass(mask_G)).astype(int)
+    # end_pos_px = np.array([0,0]) if np.any(np.isnan(center_of_mass(mask_B))) else np.round(center_of_mass(mask_B)).astype(int)
 
-    # # Locate markers at COM of mask contours - if multiple contours choose the closest to previous position
-    # # TODO - if marker crosses another contour it can switch and not recover
-    # # TODO - contour COM seems to have slightly more noise than mask COM, should be possible to get equivalent results but maybe not worth looking into
-    # # R
-    # if np.any(np.isnan(center_of_mass(mask_R))):
-    #     base_no_detection.append(count)
-    #     # previous base_pos_px will be used again
-    # else:
-    #     contours_R, _ = cv2.findContours(mask_R, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #     contour_COMs = []
-    #     for c in contours_R:
-    #         contour_COMs.append(np.mean(c.squeeze(),axis=0))
-    #     contour_COMs = np.array([np.array(contour_COMs)[:,1],np.array(contour_COMs)[:,0]]).T # Contours are [x,y] not [row,col]
-    #     base_pos_px =  contour_COMs[np.argmin(np.linalg.norm(base_pos_px-contour_COMs,axis=1))]
-    # # G
-    # if np.any(np.isnan(center_of_mass(mask_G))):
-    #     mid_no_detection.append(count)
-    #     # previous mid_pos_px will be used again
-    # else:   
-    #     contours_G, _ = cv2.findContours(mask_G, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #     contour_COMs = []
-    #     for c in contours_G:
-    #         contour_COMs.append(np.mean(c.squeeze(),axis=0))
-    #     contour_COMs = np.array([np.array(contour_COMs)[:,1],np.array(contour_COMs)[:,0]]).T
-    #     mid_pos_px = contour_COMs[np.argmin(np.linalg.norm(mid_pos_px-contour_COMs,axis=1))]
-    # # B
-    # if np.any(np.isnan(center_of_mass(mask_B))):
-    #     end_no_detection.append(count)
-    #     # previous end_pos_px will be used again
-    # else:
-    #     contours_B, _ = cv2.findContours(mask_B, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #     contour_COMs = []
-    #     for c in contours_B:
-    #         contour_COMs.append(np.mean(c.squeeze(),axis=0))
-    #     contour_COMs = np.array([np.array(contour_COMs)[:,1],np.array(contour_COMs)[:,0]]).T
-    #     end_pos_px = contour_COMs[np.argmin(np.linalg.norm(end_pos_px-contour_COMs,axis=1))]
+    # Locate markers at COM of mask contours - if multiple contours choose the closest to previous position
+    # TODO - if marker crosses another contour it can switch and not recover
+    # TODO - contour COM seems to have slightly more noise than mask COM, should be possible to get equivalent results but maybe not worth looking into
+    # R
+    if np.any(np.isnan(center_of_mass(mask_R))):
+        base_no_detection.append(count)
+        # previous base_pos_px will be used again
+    else:
+        contours_R, _ = cv2.findContours(mask_R, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_COMs = []
+        for c in contours_R:
+            contour_COMs.append(np.mean(c.squeeze(),axis=0))
+        contour_COMs = np.array([np.array(contour_COMs)[:,1],np.array(contour_COMs)[:,0]]).T # Contours are [x,y] not [row,col]
+        base_pos_px =  contour_COMs[np.argmin(np.linalg.norm(base_pos_px-contour_COMs,axis=1))]
+    # G
+    if np.any(np.isnan(center_of_mass(mask_G))):
+        mid_no_detection.append(count)
+        # previous mid_pos_px will be used again
+    else:   
+        contours_G, _ = cv2.findContours(mask_G, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_COMs = []
+        for c in contours_G:
+            contour_COMs.append(np.mean(c.squeeze(),axis=0))
+        contour_COMs = np.array([np.array(contour_COMs)[:,1],np.array(contour_COMs)[:,0]]).T
+        mid_pos_px = contour_COMs[np.argmin(np.linalg.norm(mid_pos_px-contour_COMs,axis=1))]
+    # B
+    if np.any(np.isnan(center_of_mass(mask_B))):
+        end_no_detection.append(count)
+        # previous end_pos_px will be used again
+    else:
+        contours_B, _ = cv2.findContours(mask_B, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_COMs = []
+        for c in contours_B:
+            contour_COMs.append(np.mean(c.squeeze(),axis=0))
+        contour_COMs = np.array([np.array(contour_COMs)[:,1],np.array(contour_COMs)[:,0]]).T
+        end_pos_px = contour_COMs[np.argmin(np.linalg.norm(end_pos_px-contour_COMs,axis=1))]
 
     # Convert px location to world frame
     base_pos_XZplane = UV_to_XZplane(base_pos_px[1],base_pos_px[0],Y=base_Y)
